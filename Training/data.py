d76eed9eadcb68f76b956fc328bf1ec38eab8320
Akash Haridas <58511267+akasharidas@users.noreply.github.com>
2021-11-22 19:31:06 +0530

    improved dataset loading

--- Training/data.py
+++ Training/data.py
@@ -6,15 +6,15 @@ import numpy as np
 class MRIDataset(Dataset):
     def __init__(self, path_to_data, split="train"):
         if split == "train":
-            split_indices = [*range(1, 101)]
+            split_indices = [*range(0, 101)]
         elif split == "test":
-            split_indices = [*range(101, 111)]
+            split_indices = [*range(100, 111)]
 
         print(f"Start reading {split}ing dataset...")
 
         # read the first set of volumes
-        clean_mri_set = np.load(path_to_data / "data/1.npy")
-        noisy_mri_set = np.load(path_to_data / "noisy/1.npy")
+        clean_mri_set = []
+        noisy_mri_set = []
 
         for i in tqdm_notebook(split_indices):
             # load the current volumes
@@ -22,11 +22,11 @@ class MRIDataset(Dataset):
             noisy_mri_temp = np.load(path_to_data / f"noisy/{i}.npy")
 
             # append to the existing stack
-            clean_mri_set = np.append(clean_mri_set, clean_mri_temp, axis=0)
-            noised_mri_set = np.append(noisy_mri_set, noisy_mri_temp, axis=0)
+            clean_mri_set.append(clean_mri_temp)
+            noised_mri_set.append(noisy_mri_temp)
 
-        self.clean_mri_set = clean_mri_set
-        self.noisy_mri_set = noisy_mri_set
+        self.clean_mri_set = np.concatenate(clean_mri_set, axis=0)
+        self.noisy_mri_set = np.concatenate(noisy_mri_set, axis=0)
         self.total = self.clean_mri_set.shape[0]
         self.current_patch = 1
 
